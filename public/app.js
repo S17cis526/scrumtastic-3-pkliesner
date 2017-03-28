5e219e277515b9d29ad77b54d919c1c2574ca27a
Patrick Kliesner <Patrick Kliesner>
2017-03-28 13:53:51 -0500

    After Class 3/28/17

--- public/app.js
+++ public/app.js
@@ -1,6 +1,28 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '/projects/');
-xhr.send(null);
+
+function loadIndex(){
+  $.get('/projects', function(projects, status){
+    if(status == 200){
+      $('body').clear()
+      projects.forEach(function(project){
+        var link = $('a')
+          .text(project.name)
+          .attr('href', '/projects/' + project.id)
+          .on('click', function(){
+            e.preventDefault();
+            loadProject('/projects/' + project.id);
+          });
+        $('body').append(link);
+      });
+    }
+  });
+}
+
+/*
+function loadIndex(){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/projects/');
+  xhr.send(null);
+}
 
 xhr.onreadystatechange = function() {
   var DONE = 4; // readyState 4 means the request is done.
@@ -8,20 +30,21 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
       console.log(xhr.responseText); // 'This is the returned text.'
-      var projects = JSON.parse(xhr.responseText);
-      projects.forEach(function(project){
-        var name = document.createElement('a');
-        name.innerHTML = project.name;
-        name.href = "/projects/" + project.id;
-        document.body.appendChild(name);
-        project.onClick = function(event) {
-          event.preventDefault();
-          alert("Load using Ajax");
-        }
-      });
+      var project = JSON.parse(xhr.responseText);
+      var wrapper = document.createElement('div');
+      var name = document.createElement('h1');
+      var image = document.createElement('img');
+      name.innerHTML = project.name;
+      name.href = "/projects/" + project.id;
+      document.body.appendChild(name);
+      project.onClick = function(event) {
+        event.preventDefault();
+        loadProject("/projects/" + project.id);
+      };
 
     } else {
       console.log('Error: ' + xhr.status); // An error occurred during the request.
     }
   }
 }
+*/
